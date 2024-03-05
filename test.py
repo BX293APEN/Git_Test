7ea2e9534e97d11c2426ca12f2d83a83361b7c14
PEN <uemasaki20011222@gmail.com>
2024-03-05 18:40:41 +0900

    ABC

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 if __name__ == "__main__":
     print("Hello World!!")
     print("My name is PEN!!")
-<<<<<<< HEAD
     print("TestTest")
-    a = input()
-    b = input(a) # ABC
-    print(b)
-=======
-    print("TestTestABCABCABC")
     input()
     input() # ABC
->>>>>>> ABC
 
