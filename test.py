a4cebdf31d41063df2a44823a4f751547c27d91a
PEN <uemasaki20011222@gmail.com>
2024-03-05 15:35:03 +0900

    ver.1.0.2

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
     print("Hello World!!")
+<<<<<<< HEAD
     print("My name is PEN!!")
     print("TestTest ")
+=======
+    print("My name is PEN!!")
+>>>>>>> parent of cd3525c (ver. 1.0.2)
