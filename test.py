31290b2d3271a73da26c050a5afb7c4cae70a994
PEN <uemasaki20011222@gmail.com>
2024-03-05 18:43:45 +0900

    変数追加

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ if __name__ == "__main__":
     print("Hello World!!")
     print("My name is PEN!!")
     print("TestTestABCABCABC")
-    input()
-    input() # ABC
+    a = input()
+    input(a) # ABC
 
